aed90184a17ba76bc24d4055a8c95403d7bdb6e1
Adarsh <adarshriit2017@gmail.com>
2023-06-17 01:06:41 +0530

    this should work

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app=Flask(__name__)
 def index():
     return render_template('index.html') 
 
-@app.route('/home.html',methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
