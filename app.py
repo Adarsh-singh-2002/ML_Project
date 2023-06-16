d1af84daf9c09bf5bb77dfa68adc8ad97a9ea9ce
Adarsh <adarshriit2017@gmail.com>
2023-06-17 01:19:17 +0530

    17062023

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-app=Flask(__name__)
+application=Flask(__name__)
 
-
+app=application
 
 ## Route for a home page
 
@@ -39,6 +39,12 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
+
+
 # @app.route('/predictdata',methods=['GET','POST'])
 # def predict_datapoint():
 #     if request.method=='GET':
