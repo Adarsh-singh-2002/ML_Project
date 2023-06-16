43107c2d41dee74648c62bcfc040679de51f1305
Adarsh <adarshriit2017@gmail.com>
2023-06-17 02:57:24 +0530

    work please

--- app.py
+++ app.py
@@ -15,33 +15,14 @@ app=application
 def index():
     return render_template('index.html') 
 
-# @app.route('/predictdata',methods=['GET','POST'])
-# def predict_datapoint():
-#     if request.method=='GET':
-#         return render_template('home.html')
-#     else:
-#         data=CustomData(
-#             gender=request.form.get('gender'),
-#             race_ethnicity=request.form.get('ethnicity'),
-#             parental_level_of_education=request.form.get('parental_level_of_education'),
-#             lunch=request.form.get('lunch'),
-#             test_preparation_course=request.form.get('test_preparation_course'),
-#             reading_score=float(request.form.get('writing_score')),
-#             writing_score=float(request.form.get('reading_score'))
-
-#         )
-#         pred_df=data.get_data_as_data_frame()
-#         print(pred_df)
-#         print("Before Prediction")
-
-#         predict_pipeline=PredictPipeline()
-#         print("Mid Prediction")
-#         results=predict_pipeline.predict(pred_df)
-#         print("after Prediction")
-#         return render_template('home.html',results=results[0])
-    
-
 @app.route('/predictdata', methods=['GET', 'POST'])
+def show_prediction():
+    # Calculate the predicted score
+    predicted_score = calculate_predicted_score()  # Replace this with your actual calculation logic
+
+    # Render the 'submit.html' template with the predicted score
+    return render_template('submit.html', result=predicted_score)
+
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
@@ -60,7 +41,7 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
 
-        return render_template('templates/submit.html', result=results[0])
+        return results[0]
     
 
 if __name__=="__main__":
