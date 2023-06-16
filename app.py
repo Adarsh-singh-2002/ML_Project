bd767e8da776d19434b8d180305ac1b946a825cb
Adarsh <adarshriit2017@gmail.com>
2023-06-17 02:16:03 +0530

    17062023-final commit

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
 
-        return render_template('result.html', result=results[0])
+        return render_template('submit.html', result=results[0])
     
 
 if __name__=="__main__":
@@ -91,8 +91,4 @@ if __name__=="__main__":
 #         results=predict_pipeline.predict(pred_df)
 #         print("after Prediction")
 #         return render_template('home.html',results=results[0])
-        
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
+    
